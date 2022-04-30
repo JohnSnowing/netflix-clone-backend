7f8912700eb2feaabb84e25f88afc913d1a3d2c8
Jonmari <jonmari@simplecloudology.com>
2022-05-01 05:59:03 +0800

    finish user routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRouter = require("./routes/users");
 
 dotenv.config();
 //to enable env
@@ -20,6 +21,7 @@ app.use(express.json());
 //to accept json
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRouter);
 // put all the  main routes here
 
 app.listen(8800, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 //for encrypting password crypto js
 
 router.post("/register", async (req, res) => {
@@ -38,10 +39,18 @@ router.post("/login", async (req, res) => {
     originalPassword !== req.body.password &&
       res.status(401).json("Wrong password or Username!");
 
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
+    //random secret key for accessToken
+
     const { password, ...info } = user._doc;
     //to desctructure the return to  not return the password
 
-    res.status(200).json(info);
+    res.status(200).json({ ...info, accessToken });
+    //pass the refrest token along with the mongo db retur
   } catch (error) {
     res.status(500).json(error);
   }
